248203ce2cb821eed01c13252cc6e11ae282f8fa
Ritcheli <ritchelidaniel@hotmail.com>
2024-02-14 21:03:31 -0300

    Criando novo layout para página de dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,10 +1,49 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function show_Dashboard(){
-        return view('dashboard');
+        $total_drugs_current_month = 0;
+        $total_drugs_last_month    = 0;
+
+        $query_drogas = DB::table('ocorrencias_drogas')
+                          ->select('ocorrencias_drogas.quantidade', 'ocorrencias_drogas.un_medida')
+                          ->join("ocorrencias", "ocorrencias_drogas.id_ocorrencia", "ocorrencias.id_ocorrencia")
+                          ->whereBetween('ocorrencias.data_hora', [date("Y-m-01"), date("Y-m-01", strtotime("+1 months"))])
+                          ->get();
+        
+        foreach ($query_drogas as $query_droga){
+            if ($query_droga->un_medida == "Kg") {
+                $total_drugs_current_month += $query_droga->quantidade;
+            }
+            if ($query_droga->un_medida == "g") {
+                $total_drugs_current_month += $query_droga->quantidade * 0.001;
+            }
+        }
+
+        $query_drogas = DB::table('ocorrencias_drogas')
+                          ->select('ocorrencias_drogas.quantidade', 'ocorrencias_drogas.un_medida', 'ocorrencias.data_hora')
+                          ->join("ocorrencias", "ocorrencias_drogas.id_ocorrencia", "ocorrencias.id_ocorrencia")
+                          ->whereBetween('ocorrencias.data_hora', [date("Y-m-01", strtotime("-1 months")), date("Y-m-01")])
+                          ->get();
+
+        foreach ($query_drogas as $query_droga){
+            if ($query_droga->un_medida == "Kg") {
+                $total_drugs_last_month += $query_droga->quantidade;
+            }
+            if ($query_droga->un_medida == "g") {
+                $total_drugs_last_month += $query_droga->quantidade * 0.001;
+            }
+        }
+        
+        $drugs_balance = (100 * ($total_drugs_current_month - $total_drugs_last_month))/$total_drugs_last_month;
+
+        $total_drugs_current_month = number_format($total_drugs_current_month, 1, ',', '');
+        $drugs_balance             = number_format($drugs_balance, 1, ',', '') . " %";
+
+        return view('dashboard', compact('total_drugs_current_month', 'drugs_balance'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,103 +6,89 @@
     <x-slot:other_objects></x-slot:other_objects>
 
     <x-slot:container_form>
-        <div class="container-fluid px-0">
-            <div class="row">
-                <div class="col-md-3">
-                    <div class="container-fluid DT">
-                        <div class="row">
-                            <div class="col-md-12">
-                                Drogas apreendidas
-                                <hr class="line-separator">
-                            </div>
-                            <div class="col-md-6 d-flex align-self-end">
-                                <div class="col-md pl-0 text-number">
-                                    20
-                                </div>
-                                <div class="col-md pl-0 un-medida d-flex align-self-end">
-                                    Kg
-                                </div>
-                            </div>
-                            <div class="col-md-6 d-flex justify-content-center mt-3">
-                                <img height="75" width="75" src="http://localhost:8000/uploads/icons/drug.png" alt="">
-                            </div>
-                        </div>
+        <div class="mastercontainer-dashboard">
+            <div class="container-card-data">
+                <div class="head">
+                    <div class="title">
+                        Drogas apreendidas
                     </div>
-                </div> 
-                <div class="col-md-3">
-                    <div class="container-fluid DT">
-                        <div class="row">
-                            <div class="col-md-12">
-                                Ocorrencias registradas
-                                <hr class="line-separator">
-                            </div>
-                            <div class="col-md-6 d-flex align-self-end">
-                                <div class="col-md pl-0 text-number">
-                                    100
-                                </div>
-                                <div class="col-md pl-0 un-medida d-flex align-self-end">
-                                    Un
-                                </div>
-                            </div>
-                            <div class="col-md-6 d-flex justify-content-center mt-3">
-                                <img height="75" width="75" src="http://localhost:8000/uploads/icons/ocorr.png" alt="">
-                            </div>
-                        </div>
+                    <div class="img">
+                        <img class="card-data-icon" src="{{ URL::asset('/uploads/icons/Drugs.png') }}" alt="">
                     </div>
                 </div>
-                <div class="col-md-3">
-                    <div class="container-fluid DT">
-                        <div class="row">
-                            <div class="col-md-12">
-                                Armas apreendidas
-                                <hr class="line-separator">
-                            </div>
-                            <div class="col-md-6 d-flex align-self-end">
-                                <div class="col-md pl-0 text-number">
-                                    15
-                                </div>
-                                <div class="col-md pl-0 un-medida d-flex align-self-end">
-                                    Un
-                                </div>
-                            </div>
-                            <div class="col-md-6 d-flex justify-content-center mt-3">
-                                <img height="75" width="75" src="http://localhost:8000/uploads/icons/weapon.png" alt="">
-                            </div>
-                        </div>
+                <div class="body">
+                    <div class="value">
+                        {{ $total_drugs_current_month }}
+                    </div>
+                    <div class="un_medida">
+                        Kg
                     </div>
                 </div>
-                <div class="col-md-3">
-                    <div class="container-fluid DT">
-                        <div class="row">
-                            <div class="col-md-12">
-                                Pessoas apreendidas
-                                <hr class="line-separator">
-                            </div>
-                            <div class="col-md-6 d-flex align-self-end">
-                                <div class="col-md pl-0 text-number">
-                                    5
-                                </div>
-                                <div class="col-md pl-0 un-medida d-flex align-self-end">
-                                    Pes
-                                </div>
-                            </div>
-                            <div class="col-md-6 d-flex justify-content-center mt-3">
-                                <img height="75" width="75" src="http://localhost:8000/uploads/icons/Person.png" alt="">
-                            </div>
-                        </div>
+                <div class="top-footer">
+                    <div class="top-footer-arrow">
+                        <i class='bx bx-up-arrow-alt bx-fade-down' ></i>
                     </div>
+                    Aumento de {{ $drugs_balance }}
                 </div>
-                <div class="col-md-6 mt-4 mb-6">
-                    <div class="container-fluid DT">
-                        <canvas id="chart" class="w-100" height="365px"></canvas>
-                    </div>
-                </div>
-                <div class="col-md-6 mt-4 mb-6">
-                    <div class="container-fluid DT">
-                        <canvas id="chart2" class="w-100" height="365px"></canvas>
-                    </div>
+                <div class="footer">
+                    * Comparação ao último mês
                 </div>
             </div>
+            <div class="container-card-data">
+                <div class="head">
+                    <div class="title">
+                        Pessoas apreendidas
+                    </div>
+                    <div class="img">
+                        <img style="width: 6vw; height: 11vh;" class="card-data-icon" src="{{ URL::asset('/uploads/icons/People.png') }}" alt="">
+                    </div>
+                </div>
+                <div class="body">
+                    <div class="value">
+                        10
+                    </div>
+                    <div class="un_medida">
+                        Pes
+                    </div>
+                </div>
+                <div class="top-footer">
+                    <div class="top-footer-arrow">
+                        <i class='bx bx-up-arrow-alt bx-fade-down' ></i>
+                    </div>
+                    Aumento de 10%
+                </div>
+                <div class="footer">
+                    * Comparação ao último mês
+                </div>
+            </div>
+            <div class="container-card-data">
+                <div class="head">
+                    <div class="title">
+                        Armas apreendidas
+                    </div>
+                    <div class="img">
+                        <img class="card-data-icon" src="{{ URL::asset('/uploads/icons/Weapon.png') }}" alt="">
+                    </div>
+                </div>
+                <div class="body">
+                    <div class="value">
+                        5
+                    </div>
+                    <div class="un_medida">
+                        Un
+                    </div>
+                </div>
+                <div class="top-footer">
+                    <div class="top-footer-arrow">
+                        <i class='bx bx-up-arrow-alt bx-fade-down' ></i>
+                    </div>
+                    Aumento de 10%
+                </div>
+                <div class="footer">
+                    * Comparação ao último mês
+                </div>
+            </div> 
         </div>
+        
     </x-slot:container_form>
 </x-layout>
